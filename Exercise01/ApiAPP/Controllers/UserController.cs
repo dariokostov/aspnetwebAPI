4a5ccf282e03f6cea2bf588ae9bdc423e5ff69d7
Dario Kostov <kostov.dario@gmail.com>
2019-08-21 23:55:49 +0200

    uploadEx01

---
+++ Exercise01/ApiAPP/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiAPP.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAPP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public static List<User> Users = new List<User>()
+        {
+            new User("Dario", "Kostov", 15),
+            new User("Martin", "Petrovski", 25),
+            new User("Petar", "Donevski", 32),
+            new User("Dragana", "Siskovska", 20),
+            new User("Goran", "Todorovski", 29)
+        };
+        [HttpGet]
+        public ActionResult<List<User>> Get()
+        {
+            return Users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            try
+            {
+                return Users[id - 1];
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return NotFound($"Note with id {id} is not found");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Bad request: {ex.Message}");
+            }
+        }
+
+    }
+
+}
---
+++ Exercise01/ApiAPP/Domain/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAPP.Domain
+{
+    public class User
+    {
+        public User(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
